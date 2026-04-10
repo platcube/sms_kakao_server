ac4ea36818bb81bff883080ba7b8219c962ce166
Ms-sesia <zoenea@naver.com>
2026-04-10 21:05:27 +0900

    2026-04-10 prisma.config.ts 로그 삭제 및 process.env 제외

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,6 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-// const databaseUrl = process.env.DATABASE_URL;
-// const shadowDatabaseUrl = process.env.SHADOW_DATABASE_URL;
-
-console.error("[prisma.config]", {
-  db: process.env.DATABASE_URL,
-  shadow: process.env.SHADOW_DATABASE_URL,
-});
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
