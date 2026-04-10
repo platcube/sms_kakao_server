0bae704344ce8bbac175f7a4fbfaf40b46faa0cb
Ms-sesia <zoenea@naver.com>
2026-04-10 21:08:31 +0900

    2026-04-10 다시 process.env로 되돌림

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,16 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+const databaseUrl = process.env.DATABASE_URL;
+const shadowDatabaseUrl = process.env.SHADOW_DATABASE_URL;
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+    url: databaseUrl,
+    shadowDatabaseUrl,
   },
 });
